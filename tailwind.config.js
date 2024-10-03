fb6e334695f785ee7d4ded7bd9a53218431ef45d
shettyv985 <shettyv985@gmail.com>
2024-10-03 19:35:13 +0530

    complete

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors:{
+        'primary':'#3238f2'
+      }
+    },
+  },
+  plugins: [],
+}
+
